f9e5bb68a4a6082dbd1ff6179f93de8628ee75f6
Shin Hirota <hirota@tomeapp.jp>
2021-10-04 11:52:13 +0900

    Added pydicom

--- dicom/views.py
+++ dicom/views.py
@@ -1,4 +1,7 @@
+import pydicom
+
 from django.shortcuts import render, redirect
+from django.conf import settings
 from .models import Dicom
 from .forms import DicomForm
 
@@ -6,12 +9,21 @@ def upload(request):
     if request.method == 'POST':
         form = DicomForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            saved_object = form.save()
+            json = handle_uploaded_file(settings.MEDIA_ROOT + "/" + saved_object.file.name)
+
             return render(request, 'dicom/upload_result.html', {
-                'form': form
+                'form': form,
+                'json': json,
             })
     else:
         form = DicomForm()
     return render(request, 'dicom/upload.html', {
         'form': form
     })
+
+def handle_uploaded_file(filename):
+    print(filename)
+    dataset = pydicom.filereader.dcmread(filename)
+    
+    return dataset.to_json()
